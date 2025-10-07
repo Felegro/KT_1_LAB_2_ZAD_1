dfb3d86575e92d3e69cc204d8b596766fd2a9741
Felegro <154550219+Felegro@users.noreply.github.com>
2025-10-07 17:22:23 +0500

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,11 @@
+﻿let numbers = [123; 45; 6789; 12]
+
+let getProductOfDigits (num : int) =
+    num.ToString()
+    |> Seq.map (fun c -> int c - int '0')
+    |> Seq.fold (*) 1
+
+let products = List.map getProductOfDigits numbers
+
+printfn "Исходный список цифр: %A" numbers
+printfn "Список произведений цифр: %A" products
